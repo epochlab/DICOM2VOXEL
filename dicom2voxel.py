64a11253e9a220f8a09bdd7ad03100e01bd1e030
James Roberts <James@James-Mac-Mini.local>
2022-08-07 20:04:04 +0100

    Update preview mapping

--- dicom2voxel.py
+++ dicom2voxel.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-import os
-import pydicom
+import os, pydicom
 import numpy as np
 import matplotlib.pyplot as plt
 
 def load_dicom(path):
     slices = [pydicom.dcmread(path + s) for s in sorted(os.listdir(path))]
-    slice_thickness = slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2]
-    return slices, slice_thickness
+    return slices
 
 def pixel_hu(data):
     image = np.stack([slice.pixel_array for slice in data])
@@ -22,33 +20,19 @@ def pixel_hu(data):
         image = image.astype(np.int16)
 
     image += np.int16(intercept)
-    return inp.array(image, dtype=np.int16)
+    return np.array(image, dtype=np.int16)
 
-def largest_label_volume(im, bg=-1)
-    vals, counts = np.unique(im, return_counts=True)
-    
-    counts = counts[vals != bg]
-    vals = vals[vals != bg]
-
-    if len(counts) > 0:
-        return vals[np.argmax(counts)]
-    else:
-        return None
 
 DATA = '/Volumes/artemis/library/datasets/dicom/ANGIO_CT_9712/dicom/'
 
 index = 1
-dicom, depth = load_dicom(DATA)
-pixels = pixel_hu(dicom)
 
+dicom = load_dicom(DATA)
+print(dicom[index])
 
+pixels = pixel_hu(dicom)[index]
+print(pixels)
 
-# density_min = np.amin(dicom[index].pixel_array)
-# densiry_max = np.amax(dicom[index].pixel_array)
-
-# print(dicom[index])
-# print(dicom[index].pixel_array)
-
-# plt.imshow(pixels[0], cmap=plt.cm.bone)
-# plt.colorbar()
-# plt.show()
+plt.imshow(pixels, cmap=plt.cm.bone)
+plt.colorbar()
+plt.show()
